9c0cf522bc61f8fdd7f5806ee2cc1fa119e70dbd
djo_528_1 <verizhnikoveg@gmail.com>
2024-05-14 23:43:27 +0300

    1.3.0

---
+++ JS_Labs/Lab4/lab4-1.js
@@ -0,0 +1,21 @@
+let book1 = new Object();
+book1.title = "ABC";
+book1.pubYear = 2020;
+book1.price = 893;
+let book2 = {
+    title:  "f", 
+    pubYear: 2000,
+    price: 10,
+};
+
+console.log(book1);
+console.log(book2);
+
+for (let key in book1)
+    {
+        console.log(key, ":", book1[key]);
+    }
+for (let key in book2)
+    {
+        console.log(key, ":", book2[key]);
+    }
